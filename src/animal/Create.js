d151698a695730489e8f9d67883ca7eb9c07e29d
hongquan16101995 <66354589+hongquan16101995@users.noreply.github.com>
2023-10-17 14:33:18 +0700

    yup

--- src/animal/Create.js
+++ src/animal/Create.js
@@ -1,7 +1,8 @@
 import {useState} from "react";
-import {Field, Form, Formik} from "formik";
+import {ErrorMessage, Field, Form, Formik} from "formik";
 import {create} from "./service/AnimalService"
 import {Link} from "react-router-dom";
+import * as Yup from "yup";
 
 function Create() {
     let [animal] = useState({
@@ -12,26 +13,45 @@ function Create() {
         price: "",
     })
 
+    let validate = Yup.object().shape(
+        {
+            age: Yup.number().required("Không được để trống")
+                .min(0).max(120),
+            price: Yup.number().required().min(0),
+            name: Yup.string().required().min(6)
+        }
+    )
+
     return (
         <>
             <div>
                 <Formik
                     initialValues={animal}
-                    onSubmit={(e) => createSubmit(e)}>
+                    onSubmit={(e) => createSubmit(e)}
+                    validationSchema={validate}
+                >
                     <Form>
                         <table>
                             <tbody>
                             <tr>
                                 <th><label htmlFor={'id'}>Id</label></th>
-                                <td><Field name={'id'} id={'id'}/></td>
+                                <td>
+                                    <Field name={'id'} id={'id'}/>
+                                </td>
                             </tr>
                             <tr>
                                 <th><label htmlFor={'name'}>Name</label></th>
-                                <td><Field name={'name'} id={'name'}/></td>
+                                <td>
+                                    <Field name={'name'} id={'name'}/>
+                                    <ErrorMessage name={'name'}/>
+                                </td>
                             </tr>
                             <tr>
                                 <th><label htmlFor={'age'}>Age</label></th>
-                                <td><Field name={'age'} id={'age'}/></td>
+                                <td>
+                                    <Field name={'age'} id={'age'}/>
+                                    <ErrorMessage name={'age'}/>
+                                </td>
                             </tr>
                             <tr>
                                 <th><label htmlFor={'gender'}>Gender</label></th>
@@ -39,11 +59,16 @@ function Create() {
                             </tr>
                             <tr>
                                 <th><label htmlFor={'price'}>Price</label></th>
-                                <td><Field name={'price'} id={'price'}/></td>
+                                <td>
+                                    <Field name={'price'} id={'price'}/>
+                                    <ErrorMessage name={'price'}/>
+                                </td>
                             </tr>
                             <tr>
                                 <td></td>
-                                <td><button type={'submit'}>Create</button></td>
+                                <td>
+                                    <button type={'submit'}>Create</button>
+                                </td>
                             </tr>
                             </tbody>
                         </table>
